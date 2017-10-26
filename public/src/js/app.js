0e05b26df7554e947c807656c1f3b14658b7abce
Hardik <hardikdg.sa@gmail.com>
2017-10-27 00:45:20 +0530

    Fetch and Promise example basic implementation

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,9 @@
 var defferedPrompt;
 
+if(!window.Promise){
+    window.Promise = Promise;
+}
+
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js')
     .then(function(){
@@ -12,3 +16,41 @@ window.addEventListener('beforeinstallpromot', function(event){
     event.preventDefault();
     defferedPrompt = event;
 });
+
+var promise = new Promise(function (resolve,reject){
+    setTimeout(function() {
+        // resolve("Timeout complete");
+        reject("Timeour complete");
+    }, 2000);
+})
+
+fetch('https://httpbin.org/ip').then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log(data);
+}).catch(function(error){
+    console.log(error);
+});
+
+fetch('https://httpbin.org/post', {
+    method:"POST",
+    headers: { 
+        "Content-Type": "application/json",
+        "Accept":"application/json"
+     },
+     body: JSON.stringify({message: "Sample message for the post"})
+}).then(function(response){
+    return response.json();
+}).then(function(data){
+    console.log(data);
+}).catch(function(error){
+    console.log(error);
+});
+
+promise.then(function(success) {
+    return success;
+}).then(function(newMessage){
+    console.log("Promise repeat success");
+}).catch(function(error){
+    console.log("Promise error catch");
+})
