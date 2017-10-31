4e4a0501f02744ad2fb958c0d9ebc56153638960
Hardik <hardikdg.sa@gmail.com>
2017-11-01 00:13:28 +0530

    Added static/dynamic caching Offline support with cache API

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,56 +1,24 @@
-var defferedPrompt;
 
-if(!window.Promise){
-    window.Promise = Promise;
+var deferredPrompt;
+
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js')
-    .then(function(){
-        console.log('service worker registered');
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallpromot', function(event){
-    console.log("Before install promot fired");
-    event.preventDefault();
-    defferedPrompt = event;
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
 });
-
-var promise = new Promise(function (resolve,reject){
-    setTimeout(function() {
-        // resolve("Timeout complete");
-        reject("Timeour complete");
-    }, 2000);
-})
-
-fetch('https://httpbin.org/ip').then(function(response){
-    return response.json();
-}).then(function(data){
-    console.log(data);
-}).catch(function(error){
-    console.log(error);
-});
-
-fetch('https://httpbin.org/post', {
-    method:"POST",
-    headers: { 
-        "Content-Type": "application/json",
-        "Accept":"application/json"
-     },
-     body: JSON.stringify({message: "Sample message for the post"})
-}).then(function(response){
-    return response.json();
-}).then(function(data){
-    console.log(data);
-}).catch(function(error){
-    console.log(error);
-});
-
-promise.then(function(success) {
-    return success;
-}).then(function(newMessage){
-    console.log("Promise repeat success");
-}).catch(function(error){
-    console.log("Promise error catch");
-})
